da86ee689c2689f0870935bbefc9be0ea22763e0
Sara Nolen <nolensara1@gmail.com>
2020-12-13 19:24:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime? (number)
 
-  (2..(number-1)).each do |n|
-    return false if number % n == 0
+  (-2..(number-1)).each do |n|
+    return false if number % n == 0 && n == 1
   end
   true
 end
