11e4df51bb7783e984456f12d151dbcc8f504237
Sara Nolen <nolensara1@gmail.com>
2020-12-13 19:40:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,9 @@
-# def prime? (number)
-#
-#   (-2..(number-1)).each do |n|
-#     return false if number % n == 0
-#     return false if number == 1
-#   end
-#   true
-# end
+def prime? (number)
 
-#
-# def prime?(num)
-#   #return if num <= 1
-#   (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
-# end
-
-
-  def prime(number)
+  (-2..(number-1)).each do |n|
+    return false if number % n == 0
     return false if number == 1
-    max = Math.sqrt(number)
-    (2..max).all? { |x| number % x != 0 }
+  end
+  true
 end
+
